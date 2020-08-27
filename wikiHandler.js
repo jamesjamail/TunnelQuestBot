a52ceb412e7656d472e8f1f3e42d31d5be137b59
James Jamail <james@jamesjamail.com>
2020-08-26 22:13:24 -0500

    implemented Redis for caching fetch requests

--- wikiHandler.js
+++ wikiHandler.js
@@ -3,7 +3,10 @@ const utils = require('./utils.js');
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 const aho_corasick = require('ahocorasick');
-
+const redis = require("redis");
+const cache = redis.createClient();
+ 
+const cache_expiration = 1 * 24 * 60 *60 * 1000;
 const BASE_WIKI_URL = 'http://wiki.project1999.com';
 const WTS_REGEX = /WTS(.*?)(?=WTB|$)/gi;
 const WTB_REGEX = /WTB(.*?)(?=WTS|$)/gi;
@@ -16,6 +19,10 @@ const ALL_ITEM_KEYS = new Set([
     ...Object.keys(ALIASES),
 ]);
 const SERVER_COLOR = {BLUE: '#1e1e92', GREEN: '#008000'};
+
+cache.on("error", function(error) {
+    console.error(error);
+  });
 
 async function fetchAndFormatAuctionData(auction_user, auction_contents, server) {
     const auction_wts = [...auction_contents.matchAll(WTS_REGEX)];
@@ -86,9 +93,45 @@ function formatPrice(price) {
     }
 }
 
+function parsePage(page, server) {
+    const $ = cheerio.load(page);
+    const auc_data = $(`#auc_${server} .eoTable3 td`).contents();
+    let price_data = {};
+    if (auc_data !== undefined && auc_data[0] !== undefined) {
+        price_data[30] = auc_data[0].data.trim();
+    }
+    if (auc_data !== undefined && auc_data[1] !== undefined) {
+        price_data[90] = auc_data[1].data.trim();
+    }
+    return price_data;
+}
+
 async function getWikiPricing(item_url, server) {
     // Translate server name to "capitalized" form, eg: GREEN -> Green
     server = server[0] + server.slice(1).toLowerCase();
+
+    //check cache before fetching
+    cache.get(item_url, (err, cached_data) => {
+        if (err) console.error(err);
+        //use cached data if available
+        if (cached_data) {
+            console.log('cached worked for ', item_url)
+            parsePage(cached_data, server)
+        } else {
+            //otherwise fetch new data
+            return fetch(item_url)
+                .then(response => response.text())
+                .then(text => {
+                    //store newly fetched data in cache
+                    cache.setex(item_url, cache_expiration, text, (err) => {
+                        if (err) console.error(err);
+                        //parse for price
+                        parsePage(text, server)
+                    })
+                })
+                .catch(console.error)
+        }
+    })
 
     return fetch(item_url)
         .then(response => response.text())
