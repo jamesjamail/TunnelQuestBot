9753c9d95fc6b37c3f2219a11b40e65ddf2296ee
Adam Harwell <flux.adam@gmail.com>
2023-11-19 22:59:59 -0800

    Fix items with apostrophes/backticks
    Duplicate items with apostrophes/backticks so that any version of their
    name will register (it's very unpredictable which ones are correct in
    the wiki and which are not).

--- src/lib/gameData/consolidatedItems.ts
+++ src/lib/gameData/consolidatedItems.ts
@@ -3,15 +3,31 @@ import inGameItemNamesRaw from './items.json';
 import inGameSpellNamesRaw from './spells.json';
 import inGameAliasesRaw from './aliases.json';
 
+function fixApostrophes(items: InGameItemNamesType) {
+	const duped_items: InGameItemNamesType = {};
+	for (const item of Object.entries(items)) {
+		const item_name = item[0];
+		const item_link = item[1];
+		if (item_name.indexOf('`') > 0) {
+			duped_items[item_name.replaceAll('`', '')] = item_link;
+			duped_items[item_name.replaceAll('`', "'")] = item_link;
+		} else if (item_name.indexOf("'") > 0) {
+			duped_items[item_name.replaceAll("'", '')] = item_link;
+			duped_items[item_name.replaceAll("'", '`')] = item_link;
+		}
+	}
+	return { ...items, ...duped_items };
+}
+
 export const consolidatedItemsAndAliases: InGameItemNamesType = {
-	...inGameItemNamesRaw,
-	...inGameSpellNamesRaw,
+	...fixApostrophes(inGameItemNamesRaw),
+	...fixApostrophes(inGameSpellNamesRaw),
 	...inGameAliasesRaw,
 };
 
 export const consolidatedItems: InGameItemNamesType = {
-	...inGameItemNamesRaw,
-	...inGameSpellNamesRaw,
+	...fixApostrophes(inGameItemNamesRaw),
+	...fixApostrophes(inGameSpellNamesRaw),
 };
 
 export const consolidatedItemsList = Object.keys(consolidatedItems);
