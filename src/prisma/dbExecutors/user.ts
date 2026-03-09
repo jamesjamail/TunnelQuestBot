76d942e2c20e14d96e2731bbc16588ea7fd7ddbb
Adam Harwell <flux.adam@gmail.com>
2026-03-09 06:09:14 -0400

    Clean up my fixes a bit by using upsert

--- src/prisma/dbExecutors/user.ts
+++ src/prisma/dbExecutors/user.ts
@@ -2,30 +2,13 @@ import { User as DiscordUser } from 'discord.js';
 import { prisma } from '../init';
 import { User } from '@prisma/client';
 
-export async function createUser(discordUser: DiscordUser) {
-	return prisma.user.create({
-		data: {
-			discordUserId: discordUser.id,
-			discordUsername: discordUser.username,
-		},
-	});
-}
-
 export async function findOrCreateUser(
 	discordUser: DiscordUser,
 ): Promise<User> {
-	const user = await prisma.user.findUnique({
-		where: {
-			discordUserId: discordUser.id,
-		},
-	});
-
-	if (user) {
-		return user;
-	}
-
-	return prisma.user.create({
-		data: {
+	return prisma.user.upsert({
+		where: { discordUserId: discordUser.id },
+		update: {},
+		create: {
 			discordUserId: discordUser.id,
 			discordUsername: discordUser.username,
 		},
